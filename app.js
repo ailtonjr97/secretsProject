396177c0aaee2e941b7299d0be3f37ea3e485101
Ailton de Souza Junior <ailton.souza97@hotmail.com>
2022-03-08 21:23:59 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,92 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+var encrypt = require('mongoose-encryption');
+const ejs = require("ejs");
+const _ = require("lodash");
+require('dotenv').config()
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+mongoose.connect(process.env.DB_PASSWORD);
+
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
+
+const User = mongoose.model('User', userSchema);
+
+
+app.get('/', function(req, res){
+  res.render('home');
+})
+
+///////////////////////////////////////////////////////////////
+
+app.get('/login', function(req, res){
+  res.render('login');
+})
+
+app.post('/login', function(req, res){
+  User.findOne({email: req.body.username}, function(err, foundUser){
+    if(err){
+      res.send('teste');
+    }else{
+      if(foundUser.password === req.body.password){
+        res.render('secrets');
+      } else{
+        res.send('Usuário não encontrado')
+      }
+    }
+  })
+})
+
+///////////////////////////////////////////////////////////////
+app.get('/register', function(req, res){
+  res.render('register');
+})
+
+app.post('/register', function(req, res){
+  const user = new User({
+    email: req.body.username,
+    password: req.body.password
+  })
+  user.save(function(err){
+    if(err){
+      res.send('Ocorreu um erro. Tente novamente')
+    } else{
+      res.redirect('/')
+    }
+  })
+})
+
+///////////////////////////////////////////////////////////////
+app.get('/secrets', function(req, res){
+  res.render('secrets');
+})
+
+///////////////////////////////////////////////////////////////
+app.get('/submit', function(req, res){
+  res.render('submit');
+})
+
+///////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
